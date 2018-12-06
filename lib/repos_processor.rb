ac7147d7e917becf27975a6228f348facf0285e5
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-06 18:36:14 +0000

    TTDed Repos#favourite_lang to extract langauges from repos_array

---
+++ lib/repos_processor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# ReposProcessor contains methods that extract data from an Array of Hashes
+class ReposProcessor
+
+  def favourite_lang(repos_array)
+    languages_array = []
+    repos_array.each { |json| languages_array.push(json['language']) }
+    languages_array
+  end
+end
---
+++ spec/repos_processor_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'repos_processor'
+
+describe ReposProcessor do
+  let(:repos_array) {
+    [
+      { 'language' => nil},
+      { 'language' => nil},
+      { 'language' => nil},
+      { 'language' => nil},
+      { 'language' => 'Ruby'},
+      { 'language' => 'Ruby'},
+      { 'language' => 'Ruby'},
+      { 'language' => 'Python'},
+      { 'language' => 'Elixir'}
+    ]
+  }
+  let(:processor) { described_class.new }
+
+  describe '#favourite_lang' do
+    it 'extract the languages from the repos_array' do
+      expect(processor.favourite_lang(repos_array)).to eq(
+        [nil,nil,nil,nil,'Ruby','Ruby','Ruby','Python','Elixir']
+      )
+    end
+  end
+end
