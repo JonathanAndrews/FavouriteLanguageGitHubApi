1e7da7898df9adcfba83c055787f35de41683343
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-06 20:07:56 +0000

    changed the it description on a couple to test

--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -16,13 +16,12 @@ describe UserInterface do
   let(:repos_processor) { double :ReposProcessor, favourite_lang: 'Ruby' }
 
   describe '#initialze' do
-    it 'calls #favourite_language which calls #get_repos on api_interface' do
+    it 'calls #favourite_language which calls ApiInterface#get_repos' do
       expect(api_interface).to receive(:get_repos)
         .with('octocat')
       described_class.new('octocat', api_interface, repos_processor)
     end
-    it 'calls #favourite_language,
-        which calls ReposProcessor#favourite_lang on repos_processor' do
+    it 'calls #favourite_language, which calls ReposProcessor#favourite_lang' do
       expect(repos_processor).to receive(:favourite_lang)
         .with(response_array)
       described_class.new('octocat', api_interface, repos_processor)
