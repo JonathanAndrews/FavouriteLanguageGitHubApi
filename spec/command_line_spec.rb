d8248d32bef10c68809ccbf3e9a423696fbede10
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-13 15:23:24 +0000

    One argument leads to one output

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -7,4 +7,10 @@ describe 'Running the program from the command line' do
     fav_lang.run_command
     expect(fav_lang.exitstatus).to eq 0
   end
+
+  it 'should print single fav_lang with single argument' do
+    fav_lang = Mixlib::ShellOut.new("ruby whats_their_favourite_langauge.rb octocat")
+    fav_lang.run_command
+    expect(fav_lang.stdout).to eq "octocat's favourite language is Ruby.\n"
+  end
 end
