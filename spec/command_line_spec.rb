3fcb535695313b5dbf457fd5ab541448ddc07185
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-12-13 15:22:48 +0000

    No arguments leads to no output

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -2,5 +2,9 @@ require 'mixlib/shellout'
 require_relative '../whats_their_favourite_langauge.rb'
 
 describe 'Running the program from the command line' do
-
+  it 'should nothing printed if no arguments' do
+    fav_lang = Mixlib::ShellOut.new("ruby whats_their_favourite_langauge.rb")
+    fav_lang.run_command
+    expect(fav_lang.exitstatus).to eq 0
+  end
 end
